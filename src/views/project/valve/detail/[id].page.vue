4d78d8b42cc39bdf54e2979c9dd164c5aed8dc1a
mask <617473294@qq.com>
2025-01-23 17:29:38 +0800

    aqq

---
+++ src/views/project/valve/detail/[id].page.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import { createValve, updateValve, getValveDetail } from '@/api/project/valve'
+import { useForm } from '@/components/Form'
+
+import { setSchemas } from '../data'
+import { getDeviceDetail } from '@/api/project/device'
+import dayjs from 'dayjs'
+
+const router = useRouter()
+const formType = computed(
+  () => (router.currentRoute.value.params as { id: string }).id?.split('-')[0]
+)
+const typeId = computed(() => {
+  return (router.currentRoute.value.params as { id: string }).id?.split('-')[1]
+})
+const valveDetail = ref<any>({})
+
+const getSchemas = computed(() => {
+  // 如果从 factoryId 进入
+  if (formType.value === 'factoryId') {
+    return setSchemas.filter((item) => item.path !== 'factoryId')
+  } else if (formType.value === 'deviceId') {
+    return setSchemas.filter((item) => !['factoryId', 'deviceId'].includes(item.path))
+  } else {
+    return setSchemas
+  }
+})
+const [registerForm, { validate, getPathsValue, setPathsValue }] = useForm({
+  formItem: {
+    labelWidth: 120
+  },
+  schemas: getSchemas.value
+})
+
+const handleSubmit = async () => {
+  try {
+    await validate()
+    const result = getPathsValue()
+    // factoryId.value && (result.factoryId = factoryId.value)
+    if (formType.value === 'factoryId') {
+      result.factoryId = Number(typeId.value)
+    } else if (formType.value === 'deviceId') {
+      // 获取装置id对应的最终用户id
+      const { factoryId } = await getDeviceDetail(Number(typeId.value))
+      result.factoryId = factoryId
+      result.deviceId = Number(typeId.value)
+    }
+    result.id ? await updateValve(result) : await createValve(result)
+    router.push(`/project/valve`)
+  } catch (error) {
+    console.warn(error)
+  }
+}
+
+watch(
+  () => (router.currentRoute.value.params as { id: string }).id,
+  async (val) => {
+    const result = await getValveDetail(Number(val))
+    result.since = result.since ? dayjs(result.since).valueOf() : null
+    valveDetail.value = result
+    setPathsValue(result)
+  },
+  { immediate: true }
+)
+</script>
+
+<template>
+  <div class="flex">
+    <n-card class="mr-2 w-1/4" title="编辑阀门">
+      <div>最终用户：{{ valveDetail.factory?.name }}</div>
+      <div>装置：{{ valveDetail.unit }}</div>
+      <div>位号：{{ valveDetail.tag }}</div>
+    </n-card>
+    <n-card>
+      <n-scrollbar style="max-height: 700px">
+        <Form @register="registerForm" />
+      </n-scrollbar>
+      <template #footer>
+        <n-flex justify="end">
+          <n-button @click="() => router.push(`/project/valve`)">取消</n-button>
+          <n-button type="primary" @click="handleSubmit">保存</n-button>
+        </n-flex>
+      </template>
+    </n-card>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/project/valve/runInfo/[id].page.vue
@@ -0,0 +1,105 @@
+<script setup lang="ts">
+import { getValveRunInfo, getValveDetail, type ValveRunInfo } from '@/api/project/valve'
+import { getDictDataList, getDictDataTreeListAll, getDictTypeList } from '@/api/system/dict'
+import { runInfo } from '../mock.data'
+import { cloneDeep, flattenDepth, groupBy } from 'lodash-es'
+import dayjs from 'dayjs'
+
+const router = useRouter()
+const language = ref('zh')
+const valveRunDataConfig = ref<ValveRunInfo[]>()
+const valveRunData = ref<ValveRunInfo[]>()
+const dictData = ref<any[]>([])
+const time = ref('')
+const dictDataTreeList = ref<any[]>([])
+const valveDetail = ref<any>({})
+const runInfoData = ref<ValveRunInfo[]>([])
+
+// 诊断数据
+const valueDiagnostic = ref<ValveRunInfo[]>()
+
+const transformData = (runInfoData: ValveRunInfo[]) => {
+  const data = cloneDeep(runInfoData)
+  const repeatArray = flattenDepth(
+    Object.values(groupBy(data, (i: any) => i.name)).filter((value) => value.length > 1),
+    1
+  )
+  data.forEach((item) => {
+    if (language.value === 'zh') {
+      if (repeatArray.filter((i) => i.name === item.name).length) {
+        // 重复的数据,加上数据来源
+        const source = dictDataTreeList.value.find((i) => i.id === item.treeId)?.value
+        item.name = `${item.name}(${source})`
+      }
+    } else {
+      item.name = dictData.value.find((i) => i.name === item.name)?.value || item.name
+      if (repeatArray.filter((i) => i.name === item.name).length) {
+        // 重复的数据,加上数据来源
+        const source = dictDataTreeList.value.find((i) => i.id === item.treeId)?.value
+        item.name = `${item.name}(${source})`
+      }
+    }
+  })
+  valveRunDataConfig.value = data.filter((item) => item.type === '0')
+  valveRunData.value = data.filter((item) => item.type === '1')
+  valueDiagnostic.value = data.filter((item) => item.type === '2')
+}
+
+const changeLanguage = (value: string) => {
+  language.value = value
+  transformData(runInfoData.value)
+}
+
+watch(
+  () => (router.currentRoute.value.params as { id: string }).id,
+  async (val) => {
+    valveDetail.value = await getValveDetail(Number(val))
+
+    const result = await getValveRunInfo(Number(val))
+    time.value = dayjs(result[0]?.time).format('YYYY-MM-DD') || ''
+    runInfoData.value = result
+    runInfoData.value = runInfo as any[]
+    // 对照关键字表把数据转换为对应的中英文
+    const dictType = (await getDictTypeList({ name: 'HART', pageSize: 1000 })).rows
+    const dictTypeId = dictType[0].id
+    dictData.value = (await getDictDataList({ dictTypeId, pageSize: 1000 })).rows
+    dictDataTreeList.value = await getDictDataTreeListAll()
+    transformData(runInfoData.value)
+  },
+  { immediate: true }
+)
+</script>
+
+<template>
+  <div class="flex">
+    <n-card class="mr-2 w-1/4" title="阀门运行数据详情">
+      <div>更新时间：{{ time }}</div>
+      <div>最终用户：{{ valveDetail.factory?.name }}</div>
+      <div>装置：{{ valveDetail.unit }}</div>
+      <div>位号：{{ valveDetail.tag }}</div>
+    </n-card>
+    <n-card>
+      <n-radio-group v-model:value="language" name="radiogroup" @update:value="changeLanguage">
+        <n-radio-button value="zh">中文</n-radio-button>
+        <n-radio-button value="en">English</n-radio-button>
+      </n-radio-group>
+      <n-descriptions title="阀门设置参数" label-placement="left" :column="4">
+        <n-descriptions-item v-for="item in valveRunDataConfig" :key="item.name" :label="item.name">
+          {{ item.value }} {{ item.unit ? `${item.unit}` : '' }}
+        </n-descriptions-item>
+      </n-descriptions>
+      <n-descriptions title="阀门运行数据" label-placement="left" :column="4">
+        <n-descriptions-item v-for="item in valveRunData" :key="item.name" :label="item.name">
+          {{ item.value }} {{ item.unit ? `${item.unit}` : '' }}
+        </n-descriptions-item>
+      </n-descriptions>
+      <n-descriptions title="诊断数据" label-placement="left" :column="1">
+        <n-descriptions-item v-for="item in valueDiagnostic" :key="item.name" :label="item.name">
+          {{ item.value }} {{ item.unit ? `${item.unit}` : '' }}
+        </n-descriptions-item>
+      </n-descriptions>
+    </n-card>
+  </div>
+</template>
+
+<style lang="" scoped></style>
