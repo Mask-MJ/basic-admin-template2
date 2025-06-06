f44bf123b98cd7ee3ee9110f2d80240a8c8c42f6
mask <617473294@qq.com>
2025-06-06 10:40:13 +0800

    c

--- src/views/project/valve/index.page.vue
+++ src/views/project/valve/index.page.vue
@@ -37,7 +37,7 @@ const [registerTable, { reload, getForm }] = useTable({
   columns, // 展示的列
   useSearchForm: true, // 启用搜索表单
   formConfig: { labelWidth: 100, schemas: getSchemas.value }, // 搜索表单配置
-  searchInfo: { [formType.value]: Number(typeId.value) || null }, // 额外参数
+  searchInfo: { [formType.value]: typeId }, // 额外参数
   bordered: true,
   rowKey: (rowData) => rowData.id,
   showIndexColumn: false,
@@ -187,6 +187,14 @@ const handlePositiveClick = async () => {
   await deleteAllValve()
   reload()
 }
+
+watch(
+  () => (router.currentRoute.value.params as { id: string }).id,
+  () => {
+    reload()
+  },
+  { immediate: true }
+)
 </script>
 
 <template>
